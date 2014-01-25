8536a13507152d83b18c7ec53aed62f6b4b3b21a
Anton Konoplev <kodopik@gmail.com>
2014-01-26 00:50:08 +0400

    v.0.1.1: code cleaning; new checks and conditions; possibility to index custom suffix, etc.

--- Copyroid.bash
+++ Copyroid.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyroid
-# v.0.1
+# v.0.1.1
 #
 # Makes a copy of an Android application:
 #   you will be able to install both .apk files
@@ -15,28 +15,52 @@
 # - awk
 # - convert (from ImageMagick)
 
+
+
+function printHelp() {
+    echo "Copyroid makes a copy of an Android application."
+    echo "Usage: bash ${0} file [ suffix ]"
+    echo "e.g. bash ${0} MyApplication.apk COPY"
+}
+
+
+
 HELP_LINES="-h|--help"
 if [[ ${1} =~ ${HELP_LINES} ]] ;
 then
-    echo "Makes a copy of an Android application"
-    echo "Usage: bash ${0} [apk-file]"
-    echo "e.g. bash ${0} MyApplication.apk"
+    printHelp
     exit 0
 fi
 
-ORIG_FILE=$1
-test -f "$ORIG_FILE" \
-    || exit 1
+
+
+ORIG_FILE="$1"
+SUFFIX="$2"
 ORIG_DIR='./orig'
 MANIFEST="${ORIG_DIR}/AndroidManifest.xml"
 SMALI_DIR="${ORIG_DIR}/smali"
 RES_DIR="${ORIG_DIR}/res"
 COPY_DIR="${SMALI_DIR}/copy"
 
-## Changed to `apktool -f'
-#test -d "$ORIG_DIR" && rm -r "$ORIG_DIR"
+if [[ ! -f "$ORIG_FILE" ]]
+then
+    echo "Wrong input file"
+    printHelp
+    exit 1
+fi
 
+if [[ ${SUFFIX} =~ [^a-zA-Z0-9] ]]
+then
+    echo "Suffix must be [a-zA-Z0-9]"
+    printHelp
+    exit 2
+fi
+
+
+
+# We have to add `-o' key while using 2-nd version of `apktool'
 APKTOOL_V=`apktool --version | awk -F\. '{ print $1 }'`
+
 if [[ $APKTOOL_V -gt 1 ]]
 then
     FOLDER_PARAM='-o'
@@ -44,29 +68,28 @@ else
     FOLDER_PARAM=''
 fi
 
+
+
 echo '**** DECODING ****'
 
+
+
 apktool d -f "$ORIG_FILE" $FOLDER_PARAM "$ORIG_DIR" \
-    || exit 2
+    || exit 3
 
-echo
-echo '**** CHANGING ****'
 
-PKG_NAME=`xmlstarlet sel -t -m "//manifest" -v "@package" ${MANIFEST}`
 
-## apktool v.2.x deletes versionName from manifest
-#if [[ $APKTOOL_V -gt 1 ]]
-#then
-    PKG_VERSION=`grep versionName "${ORIG_DIR}/apktool.yml" | awk -F\: '{ print $2 }'`
-    PKG_VERSION=${PKG_VERSION//[^0-9a-zA-Z\.\-]/}
-#else
-#    PKG_VERSION=`xmlstarlet sel -t -m "//manifest" -v "@android:versionName" ${MANIFEST}`
-#fi
+echo; echo '**** CHANGING ****'
 
-LABEL=`xmlstarlet sel -t -m "//application" -v "@android:label" ${MANIFEST}`
-echo $LABEL | grep -q '@string\/' && changeManifest=1 || unset changeManifest
+
 
 echo -n 'Changing app_name '
+
+PKG_NAME=`xmlstarlet sel -t -m "//manifest" -v "@package" ${MANIFEST}`
+PKG_VERSION=`grep versionName "${ORIG_DIR}/apktool.yml" | awk -F\: '{ print $2 }'`
+PKG_VERSION=${PKG_VERSION//[^0-9a-zA-Z\.\-]/}
+LABEL=`xmlstarlet sel -t -m "//application" -v "@android:label" ${MANIFEST}`
+echo $LABEL | grep -q '@string\/' && changeManifest=1 || unset changeManifest
 
 if [[ $changeManifest ]]
 then
@@ -74,44 +97,69 @@ then
     echo '(strings.xml)...'
     string=${LABEL/@string\//}
     find "$RES_DIR" -name strings\.xml \
-        -exec xmlstarlet ed -L -s "//string[@name='${string}']" -t text -n "string" -v " COPY" -n {} \;
+        -exec xmlstarlet ed -L -s "//string[@name='${string}']" -t text -n "string" -v " ${SUFFIX}" -n {} \;
 else
     # Change AndroidManifest.xml file
     echo '(AndroidManifest.xml)...'
-    xmlstarlet ed -L -u "//application[@android:label]/@android:label" -v "${LABEL} COPY" ${MANIFEST}
+    xmlstarlet ed -L -u "//application[@android:label]/@android:label" -v "${LABEL} ${SUFFIX}" ${MANIFEST}
 fi
+
+
+
+echo 'Changing icon...'
 
 ICON=`xmlstarlet sel -t -m "//application" -v "@android:icon" ${MANIFEST}`
 
-echo 'Changing icon...'
-string=${ICON/@drawable\//}
+# Icon file can be in any directory, not only res/drawable
+#string=${ICON/@drawable\//}
+string=${ICON/@*\//}
+
+# TODO: change icon effect from simple negate to something more beautiful
 find "$RES_DIR" -name "${string}\.png" \
     -exec convert {} -negate {} \;
 
-RESULT_FILE="${PKG_NAME}_${PKG_VERSION}.apk"
 
+
+echo 'Changing path...'
+
+RESULT_FILE="${PKG_NAME}_${PKG_VERSION}.apk"
 SCR_DOT_PKG=${PKG_NAME//\./\\\.}
 SCR_SLASH_PKG=${SCR_DOT_PKG//\./\/}
 SLASH_PKG=${PKG_NAME//\./\/}
-
 DIRS_PATH="${PKG_NAME//\./ }"
 MOVE_DIR="${SMALI_DIR}/${SLASH_PKG}"
 
-echo 'Changing path...'
+if [[ ! -d ${MOVE_DIR} ]]
+then
+    echo "Klutz developers!"
+    echo "Sorry, you have to make a copy of this application by hand, \'cos the clumsy developers have made the path different from ithe package name."
+    echo "Maybe, I\'ll fix it later..."
+    exit 4
+fi
+
 find ./orig -name \*\.smali \
     -exec sed -i 's/\('${SCR_SLASH_PKG}'\)/copy\/\1/g' {} \;
 
+
+
 echo 'Changing package name in smali...'
+
 find ./orig -name \*\.smali \
     -exec sed -i 's/\('${SCR_DOT_PKG}'\)/copy\.\1/g' {} \;
 
+
+
 echo 'Changing package name in xml...'
+
 find ./orig -name \*\.xml \
     -exec sed -i 's/\('${SCR_DOT_PKG}'\)/copy\.\1/g' {} \;
 
 sed -i 's/cur_package: .*/cur_package: copy\.'${PKG_NAME}'/' "${ORIG_DIR}/apktool.yml"
 
+
+
 echo 'Moving directories...'
+
 for dir in $DIRS_PATH
 do
     mkdir "${COPY_DIR}"
@@ -120,10 +168,16 @@ done
 
 mv "${MOVE_DIR}" "${COPY_DIR}"
 
-echo
-echo '**** BUILDING ****'
+
+
+echo; echo '**** BUILDING ****'
+
+
 
 apktool b "$ORIG_DIR" $FOLDER_PARAM "$RESULT_FILE" \
-    || exit 3
+    || exit 5
+
+echo; echo; echo "Your ${SUFFIX} file is \`${RESULT_FILE}\'"
+echo
 
 exit 0
